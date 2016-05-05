67c5d0e6db70bb086ac90ec77c22d0cd5e73f9f0
damien fayol <dam.fayol@gmail.com>
2016-05-05 17:34:26 +0200

    Improve style (minor)

--- src/components/Units.js
+++ src/components/Units.js
@@ -13,9 +13,12 @@ module.exports = React.createClass({
         { prefix }
         {
           this.props.units.map(function(unit, index) {
+
+            var name = unit.name.charAt(0).toUpperCase() + unit.name.slice(1);
+
             return (
               <div key={index}>
-                &nbsp;&nbsp;&nbsp;&nbsp;{unit.name} (<span className="ra  ra-crossed-swords" />{unit.attack} - {unit.hp}/{unit.maxHp}<span className="ra ra-hearts" />)
+                &nbsp;&nbsp;&nbsp;&nbsp;{name} (<span className="ra  ra-crossed-swords" />{unit.attack} - {unit.hp}/{unit.maxHp}<span className="ra ra-hearts" />)
               </div>
             );
           })
